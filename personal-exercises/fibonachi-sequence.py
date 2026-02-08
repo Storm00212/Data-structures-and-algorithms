8748574e3f32d0d8da0829ab6e8e796ba4c66f91
Storm00212 <paulmuyalikhams@gmail.com>
2026-02-08 03:33:35 +0300

    moved name abpve for loop

--- personal-exercises/fibonachi-sequence.py
+++ personal-exercises/fibonachi-sequence.py
@@ -1,13 +1,14 @@
 prev1 = 0 
 prev2 = 1
-n = 30
-
+n = 10
+print("fibonacchi sequence in python")
 for i in range(n):
-    print("fibonacchi sequence in python")
-    for j in range(current):
-        print('*', end='')
-    
     current = prev1 + prev2
     prev1 = prev2
     prev2 = current
+    
+    for j in range(current):
+        print('*', end='')
+    
+   
     
