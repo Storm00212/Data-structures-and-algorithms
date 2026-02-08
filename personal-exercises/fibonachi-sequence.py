cb8f9b950a4237c66bafaa39ca33e77d1a106be8
Storm00212 <paulmuyalikhams@gmail.com>
2026-02-08 03:31:42 +0300

    changes

--- personal-exercises/fibonachi-sequence.py
+++ personal-exercises/fibonachi-sequence.py
@@ -1,9 +1,13 @@
 prev1 = 0 
 prev2 = 1
-n = 100
+n = 30
 
 for i in range(n):
+    print("fibonacchi sequence in python")
+    for j in range(current):
+        print('*', end='')
+    
     current = prev1 + prev2
     prev1 = prev2
     prev2 = current
-    print('*' * current)
+    
